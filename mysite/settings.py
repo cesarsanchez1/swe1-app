e644fb1187691ca75926598cff9665058c109955
Cesar Sanchez <cesarsanchez@Cesars-MacBook-Pro.local>
2025-10-05 19:17:12 -0400

    Completed Django tutorial parts 1–4 with working poll app

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,15 +30,17 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'polls',  
+    "polls.apps.PollsConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
