0bffacd5fb4c831175963d21bba145772ad37a8c
Cesar Sanchez <cesarsanchez@Cesars-MacBook-Pro.local>
2025-10-06 04:56:04 -0400

    Fully functioning final version

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,64 @@
-from django.db.models import F
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
+from django.http import HttpResponse
+
+from django.template import loader
+
+from .models import Question
+
+from django.http import Http404
+
+from django.shortcuts import render
+
+from .models import Question
+
 from django.shortcuts import get_object_or_404, render
-from .models import Question, Choice
+
+from .models import Question
+
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Choice, Question
+
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
+from django.shortcuts import get_object_or_404, render
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+#def vote(request, question_id):
+#   return HttpResponse("You're voting on question %s." % question_id)
+
+# ...
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -38,4 +76,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
